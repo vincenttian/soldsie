fee5bc524c7d212f543e65a2aafa652080282503
Vincent Tian <vincenttian16@gmail.com>
2014-10-22 11:55:35 -0700

    seed some test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+def seeds
+  YAML.load(File.read(File.expand_path('../seeds.yml', __FILE__)))
+end
+
+SEEDS = seeds
+
+def create_clients
+  SEEDS['clients'].each do |client|
+  	Client.create! 	name: client['name'],
+            		root_url: client['phone'],
+            		category: client['category']
+  end
+end
+
+def create_products
+  SEEDS['products'].each do |product|
+	Product.create!  	name: product['name'],
+				        suggested_search: product['suggested_search'],
+				        suggested_url: product['suggested_url'],
+				        final_url: product['final_url']
+  end  	
+end
+
+create_clients
+create_products
